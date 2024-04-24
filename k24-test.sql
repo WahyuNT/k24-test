a5f6da13cc1ce16ac6e0087d8f62899f4a106784
Wahyu NT <wahyuntx28@gmail.com>
2024-04-24 22:06:02 +0700

    Fix typo in login.blade.php

--- k24-test.sql
+++ k24-test.sql
@@ -5,132 +5,129 @@
 -- HeidiSQL Version:             12.5.0.6677
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET NAMES utf8 */
+;
+/*!50503 SET NAMES utf8mb4 */
+;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+;
+/*!40103 SET TIME_ZONE='+00:00' */
+;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+;
 
 -- Dumping database structure for k24-test
 CREATE DATABASE IF NOT EXISTS `k24-test` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+
 USE `k24-test`;
 
 -- Dumping structure for table k24-test.admins
 CREATE TABLE IF NOT EXISTS `admins` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `foto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `nama_lengkap` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT, `foto` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `nama_lengkap` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `username` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `created_at` timestamp NULL DEFAULT NULL, `updated_at` timestamp NULL DEFAULT NULL, PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.admins: ~1 rows (approximately)
-INSERT INTO `admins` (`id`, `foto`, `nama_lengkap`, `username`, `password`, `created_at`, `updated_at`) VALUES
-	(1, 'admin1.png', 'admin', 'admin', 'admin', NULL, '2024-04-24 04:58:16');
+INSERT INTO
+    `admins` (
+        `id`, `foto`, `nama_lengkap`, `username`, `password`, `created_at`, `updated_at`
+    )
+VALUES (
+        1, 'admin1.png', 'admin', 'admin', 'admin', NULL, '2024-04-24 04:58:16'
+    );
 
 -- Dumping structure for table k24-test.failed_jobs
 CREATE TABLE IF NOT EXISTS `failed_jobs` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT, `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `connection` text COLLATE utf8mb4_unicode_ci NOT NULL, `queue` text COLLATE utf8mb4_unicode_ci NOT NULL, `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL, `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL, `failed_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (`id`), UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.failed_jobs: ~0 rows (approximately)
 
 -- Dumping structure for table k24-test.members
 CREATE TABLE IF NOT EXISTS `members` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `no_hp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `no_ktp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `jenis_kelamin` enum('L','P') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `members_email_unique` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT, `foto` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL, `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `no_hp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `no_ktp` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `tanggal_lahir` date NOT NULL, `jenis_kelamin` enum('L', 'P') COLLATE utf8mb4_unicode_ci NOT NULL, `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `created_at` timestamp NULL DEFAULT NULL, `updated_at` timestamp NULL DEFAULT NULL, PRIMARY KEY (`id`), UNIQUE KEY `members_email_unique` (`email`)
+) ENGINE = InnoDB AUTO_INCREMENT = 17 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.members: ~4 rows (approximately)
-INSERT INTO `members` (`id`, `foto`, `name`, `email`, `no_hp`, `no_ktp`, `tanggal_lahir`, `jenis_kelamin`, `password`, `created_at`, `updated_at`) VALUES
-	(1, '1713964409_monkey-artwork-illustration-on-light-sky-background-free-vector.jpg', 'Mone', 'mone@gmail.com', '123', '1234', '2024-04-02', 'L', '123456', NULL, '2024-04-24 06:19:13'),
-	(4, '1713964714_ava3.jpg', 'Rama', 'rama@gmail.com', '12345', '12356', '2024-02-12', 'L', '222', NULL, '2024-04-24 06:19:35'),
-	(5, '1713964703_ava4.jpeg', 'Asa', 'asa@gmail.com', '12343534', '12334534', '2024-03-02', 'P', '222', NULL, '2024-04-24 06:19:48'),
-	(16, '1713964725_ava.png', 'Raka', 'raka@gmail.com', '234322', '2423', '2022-01-04', 'P', '123456', NULL, '2024-04-24 06:20:17');
+INSERT INTO
+    `members` (
+        `id`, `foto`, `name`, `email`, `no_hp`, `no_ktp`, `tanggal_lahir`, `jenis_kelamin`, `password`, `created_at`, `updated_at`
+    )
+VALUES (
+        1, '1713965617_ava2.jpg', 'Mone', 'mone@gmail.com', '123', '1234', '2024-04-02', 'L', '123456', NULL, '2024-04-24 06:19:13'
+    ),
+    (
+        4, '1713964714_ava3.jpg', 'Rama', 'rama@gmail.com', '12345', '12356', '2024-02-12', 'L', '222', NULL, '2024-04-24 06:19:35'
+    ),
+    (
+        5, '1713964703_ava4.jpeg', 'Asa', 'asa@gmail.com', '12343534', '12334534', '2024-03-02', 'P', '222', NULL, '2024-04-24 06:19:48'
+    ),
+    (
+        16, '1713964725_ava.png', 'Raka', 'raka@gmail.com', '234322', '2423', '2022-01-04', 'P', '123456', NULL, '2024-04-24 06:20:17'
+    );
 
 -- Dumping structure for table k24-test.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` int unsigned NOT NULL AUTO_INCREMENT, `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `batch` int NOT NULL, PRIMARY KEY (`id`)
+) ENGINE = InnoDB AUTO_INCREMENT = 7 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.migrations: ~0 rows (approximately)
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-	(1, '2014_10_12_100000_create_password_resets_table', 1),
-	(2, '2019_08_19_000000_create_failed_jobs_table', 1),
-	(3, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-	(4, '2024_04_23_172059_create_users_table', 1),
-	(5, '2024_04_23_172351_create_admins_table', 1),
-	(6, '2024_04_23_172601_create_members_table', 1);
+INSERT INTO
+    `migrations` (`id`, `migration`, `batch`)
+VALUES (
+        1, '2014_10_12_100000_create_password_resets_table', 1
+    ),
+    (
+        2, '2019_08_19_000000_create_failed_jobs_table', 1
+    ),
+    (
+        3, '2019_12_14_000001_create_personal_access_tokens_table', 1
+    ),
+    (
+        4, '2024_04_23_172059_create_users_table', 1
+    ),
+    (
+        5, '2024_04_23_172351_create_admins_table', 1
+    ),
+    (
+        6, '2024_04_23_172601_create_members_table', 1
+    );
 
 -- Dumping structure for table k24-test.password_resets
 CREATE TABLE IF NOT EXISTS `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  KEY `password_resets_email_index` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `created_at` timestamp NULL DEFAULT NULL, KEY `password_resets_email_index` (`email`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.password_resets: ~0 rows (approximately)
 
 -- Dumping structure for table k24-test.personal_access_tokens
 CREATE TABLE IF NOT EXISTS `personal_access_tokens` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `tokenable_id` bigint unsigned NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `abilities` text COLLATE utf8mb4_unicode_ci,
-  `last_used_at` timestamp NULL DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
-  KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT, `tokenable_type` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `tokenable_id` bigint unsigned NOT NULL, `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `token` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL, `abilities` text COLLATE utf8mb4_unicode_ci, `last_used_at` timestamp NULL DEFAULT NULL, `created_at` timestamp NULL DEFAULT NULL, `updated_at` timestamp NULL DEFAULT NULL, PRIMARY KEY (`id`), UNIQUE KEY `personal_access_tokens_token_unique` (`token`), KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (
+        `tokenable_type`, `tokenable_id`
+    )
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.personal_access_tokens: ~0 rows (approximately)
 
 -- Dumping structure for table k24-test.users
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+    `id` bigint unsigned NOT NULL AUTO_INCREMENT, `created_at` timestamp NULL DEFAULT NULL, `updated_at` timestamp NULL DEFAULT NULL, PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
 -- Dumping data for table k24-test.users: ~0 rows (approximately)
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */
+;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */
+;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */
+;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */
+;
